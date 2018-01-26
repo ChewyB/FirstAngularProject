9db72c04f31d4d917757933ba7863de45eda72a3
ChewyB <j.e.beltranq@gmail.com>
2018-01-26 00:38:22 -0800

    Wrapped up section 3. This includes the test recipe, detail and shopping list and edit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+recipes: Recipe[] = [
+  new Recipe('TestRecipe', 'TestDescription', 'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif'),
+  new Recipe('TestRecipe', 'TestDescription', 'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif')
+];
 
   constructor() { }
 
